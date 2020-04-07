9e967148e402fb467f58cc3b6d2947621c8d4b5c
RichardEsquivel <rlesquivel@gmail.com>
2020-04-07 17:26:18 -0500

    Created insert function with error warning

--- scratchpad.py
+++ scratchpad.py
@@ -7,7 +7,9 @@ a = [[8, 4], [90, -1, 3], [9, 62], [-7, -1, -56, -6], [201], [76, 18]]
 
 
 def minArraySums(arr):
+    # set sum to 0
     sum = 0
+# calculate number of elements to to iterate through using range
     n = len(arr)
     for i in range(n):
         sum += min(arr[i])
